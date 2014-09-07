c7e7573f6672ebc0f3000033d92966f130f12abe
Marcel <otte.mm@googlemail.com>
2014-09-07 18:06:39 +0200

    wip: exceptions, further parsers

---
+++ src/to/mmo/adis/parser/ADISParseException.java
@@ -0,0 +1,22 @@
+package to.mmo.adis.parser;
+
+@SuppressWarnings("serial")
+public class ADISParseException extends Exception {
+
+	private String why;
+	private String failedData;
+	private int position;
+
+	public ADISParseException(String failedData, int position, String why) {
+		super(why);
+		this.failedData = failedData;
+		this.position = position;
+		this.why = why;
+	}
+
+	public ADISParseException(String why) {
+		super(why);
+		this.why = why;
+	}
+
+}
---
+++ src/to/mmo/adis/parser/RequestParser.java
@@ -0,0 +1,5 @@
+package to.mmo.adis.parser;
+
+public class RequestParser {
+
+}
