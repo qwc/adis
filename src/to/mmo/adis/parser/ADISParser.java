fbb8253aaa32fc2f65b29ec2b6d08b79b2f682ef
Marcel <otte.mm@googlemail.com>
2014-09-13 18:47:08 +0200

    added handler handling and rudimentary exception throwing

--- src/to/mmo/adis/parser/ADISParser.java
+++ src/to/mmo/adis/parser/ADISParser.java
@@ -14,9 +14,11 @@ import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.regex.Pattern;
 
 import to.mmo.adis.EntityValue;
+import to.mmo.adis.handler.EntityHandler;
 
 public class ADISParser extends Thread {
 	// ADIS = Agricultural Data Interchange Syntax
@@ -29,6 +31,7 @@ public class ADISParser extends Thread {
 	private ArrayList<EntityValue> parsedEntities;
 	private FinishCondition condition;
 	private LineState currentLineState;
+	private ConcurrentHashMap<String, EntityHandler> entityHandlers;
 
 	public static enum ParserStates {
 		HEADER, DATA, END, FAILURE
@@ -55,6 +58,7 @@ public class ADISParser extends Thread {
 	}
 
 	public ADISParser() {
+		entityHandlers = new ConcurrentHashMap<String, EntityHandler>();
 		System.out.println("constructing parser");
 		status = ParserStates.HEADER;
 		parsedEntities = new ArrayList<EntityValue>();
@@ -113,7 +117,7 @@ public class ADISParser extends Thread {
 		}
 	}
 
-	private Object parseLine(String line) {
+	private void parseLine(String line) throws ADISParseException {
 		// TODO: throw parse exception with error information in adis/aded
 		// syntax also put that on the output stream.
 		// Need to think about the return objects...
@@ -128,16 +132,34 @@ public class ADISParser extends Thread {
 		if (currentLineState == LineState.D) {
 			eiParser = new EntityItemParser();
 			if (eiParser.parseDefinition(line) == null) {
-				return null;
+				throw new ADISParseException("", 0, "");
 			}
 		}
 		if (currentLineState == LineState.V) {
-			return eiParser.parse(line);
+			EntityValue ev = eiParser.parse(line);
+			EntityHandler h;
+			if ((h = entityHandlers.get(ev.getEntity())) != null) {
+				h.handle(ev);
+			}
 		}
-		return null;
+		if (currentLineState == LineState.S || currentLineState == LineState.R) {
+			// TODO read all search lines (for filtering)
+			// the request lines
+			// a done requirement is needed: either a RO line or a TN line
+			// and now give that resulting object thingy to a request handler...
+			// o.O
+		}
 	}
 
 	public ArrayList<EntityValue> getParsedEntities() {
 		return parsedEntities;
+	}
+
+	public void addEntityHandler(EntityHandler handler) {
+		entityHandlers.put(handler.entity(), handler);
+	}
+
+	public void removeEntityHandler(EntityHandler handler) {
+		entityHandlers.remove(handler.entity());
 	}
 }
