e65abf41e83d6a0ec5cb70496b069b3bec61ce60
Marcel <otte.mm@googlemail.com>
2014-09-18 20:05:26 +0200

    content for request and search value class

--- src/to/mmo/adis/RequestValue.java
+++ src/to/mmo/adis/RequestValue.java
@@ -1,6 +1,7 @@
 package to.mmo.adis;
 
 import java.util.ArrayList;
+import java.util.List;
 
 public class RequestValue {
 
@@ -8,4 +9,37 @@ public class RequestValue {
 	private EntityValue entity;
 	private int maxEntries;
 
+	public RequestValue() {
+		this.searchFilters = new ArrayList<SearchValue>();
+	}
+
+	public RequestValue(EntityValue entity, int maxEntries) {
+		this();
+		this.entity = entity;
+		this.maxEntries = maxEntries;
+	}
+
+	public EntityValue getEntity() {
+		return entity;
+	}
+
+	public void setEntity(EntityValue entity) {
+		this.entity = entity;
+	}
+
+	public int getMaxEntries() {
+		return maxEntries;
+	}
+
+	public void setMaxEntries(int maxEntries) {
+		this.maxEntries = maxEntries;
+	}
+
+	public void addSearchValue(SearchValue value) {
+		searchFilters.add(value);
+	}
+
+	public List<SearchValue> getSearchValues() {
+		return searchFilters;
+	}
 }
--- src/to/mmo/adis/SearchValue.java
+++ src/to/mmo/adis/SearchValue.java
@@ -1,9 +1,34 @@
 package to.mmo.adis;
 
-
 public class SearchValue {
 
 	private EntityValue entity;
 	private int maxEntries;
+
+	public SearchValue() {
+
+	}
+
+	public SearchValue(EntityValue entity, int maxentries) {
+		this();
+		this.entity = entity;
+		maxEntries = maxentries;
+	}
+
+	public EntityValue getEntity() {
+		return entity;
+	}
+
+	public void setEntity(EntityValue entity) {
+		this.entity = entity;
+	}
+
+	public int getMaxEntries() {
+		return maxEntries;
+	}
+
+	public void setMaxEntries(int maxEntries) {
+		this.maxEntries = maxEntries;
+	}
 
 }
