0c2e7b094519994a3c4ef54200180c0a2a35744d
Marcel <otte.mm@googlemail.com>
2014-09-18 20:08:12 +0200

    defined a global class for global ADIS information

---
+++ src/to/mmo/adis/ADIS.java
@@ -0,0 +1,14 @@
+package to.mmo.adis;
+
+public class ADIS {
+	// Container class for enums and other global things...
+
+	public static enum Status {
+		H, N, S, F, D
+	}
+
+	public static enum LineType {
+		D, V, E, C, S, R, F, I, O, T, Z
+	}
+
+}
